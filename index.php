5354f2943df1fc016fc74c287b685b70a247b9b5
Bryan Mares <bryanmaresdev@outlook.com>
2022-12-13 00:30:26 -0600

    más funcionalidad

--- index.php
+++ index.php
@@ -72,13 +72,25 @@
     </div>
     <div class="principal">
       <div class="mes">
-        <div class="botones">
-          <select name="mes" id="mes">
-            <option value=""></option>
+        <div class="selects">
+          <select name="mes" id="mes" class="select-css">
+            <option value="01">Enero</option>
+            <option value="02">Febrero</option>
+            <option value="03">Marzo</option>
+            <option value="04">Abril</option>
+            <option value="05">Mayo</option>
+            <option value="06">Junio</option>
+            <option value="07">Julio</option>
+            <option value="08">Agosto</option>
+            <option value="09">Septiembre</option>
+            <option value="10">Octubre</option>
+            <option value="11">Noviembre</option>
+            <option value="12">Diciembre</option>
           </select>
-        </div>
-        <div class="mes-nombre">
-          Diciembre
+          <select name="anio" id="anio" class="select-css">
+            <option value="0">Año</option>
+            <?php  for($i=1990;$i<=2030;$i++) { echo "<option value='".$i."'>".$i."</option>"; } ?>
+          </select>
         </div>
         <div class="agregar">
           <button class="btn-agregar">
